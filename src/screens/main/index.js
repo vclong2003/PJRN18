8f5505725ff81ca6c2c090570275f05533966df4
vclong2003 <vclong2003@gmail.com>
2022-02-05 20:28:43 +0700

    ok

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,32 +1,48 @@
 import {StyleSheet, Text, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import React from 'react';
 
 const MainScreen = () => {
   return (
-    <View style={{flex: 1, backgroundColor: 'white', display: 'flex'}}>
-      <View style={{flex: 3, backgroundColor: 'black'}}>
-        <View style={{flex: 3, backgroundColor: 'red'}}>
-          <View style={{flex: 1, backgroundColor: 'blue'}}></View>
-          <View
-            style={{
-              flex: 2,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+    <LinearGradient
+      colors={['#4c669f', '#3b5998', '#192f6a']}>
+      <View style={{flex: 1, backgroundColor: 'white', display: 'flex'}}>
+        <View style={{flex: 3}}>
+          <View style={{flex: 3}}>
             <View
               style={{
-                backgroundColor: 'black',
-                width: 230,
-                height: 230,
-                borderRadius: 240,
-              }}></View>
+                flex: 1,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+              }}>
+              <Text style={{fontSize: 14}}>Today, May 7th, 2021</Text>
+              <View style={{display: 'flex', alignItems: 'center'}}>
+                <Text style={{fontSize: 40, fontWeight: '700'}}>Barcelona</Text>
+                <Text style={{fontSize: 20}}>Spain</Text>
+              </View>
+            </View>
+            <View
+              style={{
+                flex: 2,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  backgroundColor: 'black',
+                  width: 230,
+                  height: 230,
+                  borderRadius: 240,
+                }}></View>
+            </View>
           </View>
+          <View style={{flex: 1.2, backgroundColor: 'green'}}></View>
         </View>
-        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+        <View style={{flex: 1.1, backgroundColor: 'white'}}></View>
       </View>
-      <View style={{flex: 1, backgroundColor: 'white'}}></View>
-    </View>
+    </LinearGradient>
   );
 };
 
